c1bd90023a7d6e582dd15dab2424fc477cb4a748
Junio Becker <143370733+juniobecker@users.noreply.github.com>
2024-09-09 22:09:13 -0300

    Desafio nacionalidade.

--- exercicios/aula11/ex008.js
+++ exercicios/aula11/ex008.js
@@ -1,6 +1,6 @@
 var vel = 12
 console.log(`A velocidade do seu carro é ${vel}Km/h `)
-if(vel > 60) {
+if(vel > 60) { //Caso não tenha o 'se não'(esle) a condição é chamada de simples.
 
     console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
 } else {
