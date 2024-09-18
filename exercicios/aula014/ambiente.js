781cacdd4fa184795799e71ab4a8fc3dbbe7841e
Junio Becker <143370733+juniobecker@users.noreply.github.com>
2024-09-17 22:12:55 -0300

    .

--- exercicios/aula014/ambiente.js
+++ exercicios/aula014/ambiente.js
@@ -1,13 +1,6 @@
 
-/*
-var c = 1
-while (c <= 5) {
-    console.log(c)
-    c++
-}
-*/
-//---------------------------
-
-for(var c = 1; (c <= 500000000); c++) {
+console.log('Vai começar...')
+for(var c = 1; (c <= 4); c++) {
     console.log(c)
 }
+console.log('FIM!')
